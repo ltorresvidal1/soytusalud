46a4aedb40faf7f71860d33eab5d5dd94adffb06
lilianaromerop <lilianaromerop11@gmail.com>
2022-04-23 16:31:06 -0500

    armando formulario registro

--- pages/registro.jsx
+++ pages/registro.jsx
@@ -6,7 +6,7 @@ const registro = () => {
   return (
     <>
     <div>
-    <section className="promo-primary">
+          <section className="promo-primary">
             <picture className="img--bg">
               <Image src={Promo1} alt="img" width={"3000vw"} layout="responsive"/>
               </picture>
@@ -22,27 +22,26 @@ const registro = () => {
                 </div>
               </div>
           </section>
-            
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-          </div>
-          <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
 
+          <section className="mx-auto mt-12 mb-12 bg-white drop-shadow-xl w-2/5">
+          <form className="p-4">
+              <div class="form-group">
+                <label for="idtype">Tipo de documento</label>
+                <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+              </div>
+             
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </section>
     </div>
-       
-        
     </>
   )
 }
