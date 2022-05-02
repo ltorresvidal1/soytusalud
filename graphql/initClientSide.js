e02a7f52dc144e57e687a9c5cdbd4f5f71d4da27
iSebastianCabrera <jscrojas25@yahoo.es>
2022-05-02 10:49:32 -0500

    lunch

--- graphql/initClientSide.js
+++ graphql/initClientSide.js
@@ -2,6 +2,6 @@ import { ApolloClient , InMemoryCache } from "@apollo/client";
 
 
 export const client = new ApolloClient({
-    uri: "http://127.0.0.1:3000/api/graphql",
+    uri: process.env.NEXT_PUBLIC_API_GRPAPHQL,
     cache: new InMemoryCache(),
 });
