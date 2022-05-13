03f05f553cf419b4c9cb0b6300f36476191e20a6
lilianaromerop <lilianaromerop11@gmail.com>
2022-05-13 18:05:43 -0500

    ajustando css nuevo

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,66 +11,49 @@ export default function Home() {
       <div>
           <Box sx={{backgroundColor:"#F9F7F6"}}id="about">
             <Container maxWidth="xl">
-              <div className="flex ">
-                <div className="col-lg-6 flex-grow">
-                    <span className='text-gray-400 font-semibold text-xl'>Sobre Nosotros</span>
-                    <Typography fontFamily={'Quicksand'} sx={{color:'#4A4C70'}} className='text-5xl font-extrabold'>Soy Tú Salud</Typography>
+                <div className="mt-20 flex justify-center space-x-32">
                   <div>
-                  <p>Conoces a algún familiar, amigo, persona o tú que necesite algún servicio médico y se encuentre en situación de vulnerabilidad? En www.soytusalud.org informa tu historia o su historia, aplica y Listo!!.</p><p> En la Fundación <a href="">www.soytusalud.org</a> una vez has demostrado que estás en una situación de vulnerabilidad, podrás acceder :</p>
-                    <ul className="">
-                      <li>Consulta Médica General por Telemedicina.</li>
-                      <li>Consulta Médica Especializada por Telemedicina.</li>
-                      <li>Medicamentos para enfermedades no crónicas.</li>
-                      <li>Exámenes de Laboratorio Domiciliarios.</li>
-                      <li>Otros servicios para su salud, que estén disponibles.</li>
-                      <a className="" href="#">Más sobre nosotros</a>
-                    </ul>
+                  <span className='text-gray-400 font-semibold text-xl'>Sobre Nosotros</span>
+                      <Typography fontFamily={'Quicksand'} sx={{color:'#4A4C70'}} className='text-5xl font-extrabold'>Soy Tú Salud</Typography>
+                    <div>
+                      <p className="max-w-2xl mt-8">Conoces a algún familiar, amigo, persona o tú que necesite algún servicio médico y se encuentre en situación de vulnerabilidad? En www.soytusalud.org informa tu historia o su historia, aplica y Listo!!.<p> <br/>En la Fundación <a href="">www.soytusalud.org</a> una vez has demostrado que estás en una situación de vulnerabilidad, podrás acceder :</p></p>
+                        <ul className="">
+                          <li>Consulta Médica General por Telemedicina.</li>
+                          <li>Consulta Médica Especializada por Telemedicina.</li>
+                          <li>Medicamentos para enfermedades no crónicas.</li>
+                          <li>Exámenes de Laboratorio Domiciliarios.</li>
+                          <li>Otros servicios para su salud, que estén disponibles.</li>
+                          <br/>
+                          <a className="bg-amber-300 rounded-full p-4" href="#">Más sobre nosotros</a>
+                        </ul>
+                    </div>
                   </div>
-                </div>
-                <div className="">
-                  <div className="">
+                  <div className="flex-col">
                     <div className="absolute">
-                      <Image  src="/about_layout.png" alt="img" width={700} height={700}/>
+                      <Image src="/about_layout.png" alt="img" width={650} height={650}/>
                     </div>
-                    <div className="">
-                      <Image src="/gallery_2.png" alt="img" width={350} height={350}/>
+                    <div className="relative">
+                      <Image src="/gallery_2.png" alt="img" width={380} height={380}/>
                     </div>
                   </div>
-                </div>  
-                </div>
+                </div> 
             </Container>
           </Box>
-                    
-          <section className="section icons-section no-padding-top">
-            <div className="container">
-              <div className="row margin-bottom">
-                <div className="col-12">
-                  <div className="heading heading--center"><span className="heading__pre-title">Servicios</span>
-                    <h2 className="heading__title"><span>Lo que hacemos</span> <span>para todas las personas</span> </h2>
-                  </div>
+          <Box className="mt-40 text-center">
+            <div className="col-12">
+              <div className="icon icon-item__icon icon--red"> 
+                <div className="img--layout" >
+                </div>
+                <Image src="/Logo_servicios1.png" alt="img" width={200} height={200}/>
+                </div>
+                <div className="heading heading--center"><span className="heading__pre-title">Servicios</span>
+                  <h2 className="heading__title"><span>Lo que hacemos</span> <span>para todas las personas</span> </h2>
+                </div>
+                <div className="icon-item__text">
+                  <p>Ayuda Médica</p>
                 </div>
               </div>
-              <div className="row">
-                <div className="col-12 col-lg-12">
-                  <div className="icon-item">
-                    <div className="icon-item__img">
-                    <div className="icon icon-item__icon icon--red"> 
-                    <Image src="/Logo_servicios1.png" alt="img"  width={300} height={300}/>
-                    </div>
-                    
-                    <div  className="img--layout" >
-                      <Image src="/icon_1-1.png" alt="img" width={200} height={200}/>
-                      </div>
-                                                  
-                    </div>
-                    <div className="icon-item__text">
-                      <p>Ayuda Medica</p>
-                    </div>
-                  </div>
-                </div>                             
-              </div>
-            </div>
-          </section>
+          </Box>
       </div>
     </LayoutMain>
   )
---
+++ pages/private/admin/comunidades/index 3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PrivateLayout from '../../../../components/layouts/PrivateLayout'
+
+const Index = () => {
+  return (
+    <PrivateLayout>
+        <div>Index</div>
+    </PrivateLayout>
+  )
+}
+
+export default Index
