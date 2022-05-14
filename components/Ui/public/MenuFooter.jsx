a8ba8ef9b21c0cae847b584b803e5d835d6ca384
lilianaromerop <lilianaromerop11@gmail.com>
2022-05-13 22:30:42 -0500

    terminando footer, falta responsive

--- components/Ui/public/MenuFooter.jsx
+++ components/Ui/public/MenuFooter.jsx
@@ -17,37 +17,40 @@ export const MenuFooter = () => {
 
 </Box>
  <Box>
- <footer className='bg-zinc-800'>
-
-                  <div>
-                      <div className="footer-logo"><a className="footer-logo__link"><Image className="" src="/Logo_P4.png"  width="150" height="150" alt="logo"/></a></div>
-                  </div> 
-                    <ul>
-                        <li className="footer-socials__item"><a className="footer-socials__link" href="https://www.facebook.com/Fundación-Soy-Tú-Salud-107327561900263" target="_blank" rel="noreferrer"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li className="footer-socials__item"><a className="footer-socials__link" href="https://www.linkedin.com/in/fundación-soy-tú-salud-845592233/" target="_blank" rel="noreferrer"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                        <li className="footer-socials__item"><a className="footer-socials__link" href="https://twitter.com/FundacinSoyTSa1" target="_blank" rel="noreferrer"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li className="footer-socials__item"><a className="footer-socials__link" href="https://www.instagram.com/fundacionsoytusalud/" target="_blank" rel="noreferrer"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-                    </ul>
-
-              <div className="col-sm-6 col-lg-3">
-                <span className='text-white font-bold text-lg'>Contáctanos</span>
-                  <div className="footer-contacts">
-                      <p className="footer-contacts__address">Medellín</p>
-                      <p className="footer-contacts__phone">Teléfono: <a href="tel:+3207148401">+57 320 702 3823</a></p>
-                      <p className="footer-contacts__mail">Email: <a href="mailto:soutu@soutu.org">info@soytusalud.org</a></p>
+ <footer className='bg-zinc-800 flex justify-center space-x-60'>
+              <div className='flex'>
+                <div>
+                    <div className="footer-logo"><a className="footer-logo__link"><Image className="" src="/Logo_P4.png"  width="150" height="150" alt="logo"/></a></div>
+                </div>
+                <div>
+                  <span className='text-white font-bold text-lg'>Contáctanos</span>
+                  <div className="flex flex-col text-gray-500 tracking-tight font-semibold">
+                      <span className="footer-contacts__address">Medellín</span>
+                      <span className="footer-contacts__phone">Teléfono: <a href="tel:+3207148401">+57 320 702 3823</a></span>
+                      <span className="footer-contacts__mail">Email: <a href="mailto:soutu@soutu.org">info@soytusalud.org</a></span>
                   </div>
+
+                </div>
+                
               </div>
-              <div>
+              <div className="flex flex-col">
                 <span className='text-white font-bold text-lg'>Te acompaño</span>
-                <p>Ayúdanos a cambiar la vida de los menos favorecidos en Colombia</p><a className="button footer__button button--filled" href="#">TE ACOMPAÑO</a>
-              </div>
-              <div className="col-md-6">
-                  <p className="footer-copyright">Copyrights Soy Tú © 2022. Todos los derechos reservado.</p>
+                <span className='text-gray-500 tracking-tight font-semibold'>Ayúdanos a cambiar la vida de los menos favorecidos en Colombia</span>
+                <button className="mt-10 bg-amber-300 hover:drop-shadow-md rounded-full p-4 cursor-pointer px-12 uppercase text-xs font-bold" href="#">Más sobre nosotros</button>
               </div>
   </footer>
+  <div className="col-md-6">
+      <span className="footer-copyright">Copyrights Soy Tú © 2022. Todos los derechos reservado.</span>
+  </div>
  </Box>
     
 
 </>
   )
 }
+
+
+{/* <li className="footer-socials__item"><a className="footer-socials__link" href="https://www.facebook.com/Fundación-Soy-Tú-Salud-107327561900263" target="_blank" rel="noreferrer"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+<li className="footer-socials__item"><a className="footer-socials__link" href="https://www.linkedin.com/in/fundación-soy-tú-salud-845592233/" target="_blank" rel="noreferrer"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+<li className="footer-socials__item"><a className="footer-socials__link" href="https://twitter.com/FundacinSoyTSa1" target="_blank" rel="noreferrer"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+<li className="footer-socials__item"><a className="footer-socials__link" href="https://www.instagram.com/fundacionsoytusalud/" target="_blank" rel="noreferrer"><i className="fa fa-instagram" aria-hidden="true"></i></a></li> */}
