5f186d29dcc6a7ea858f20b940b9bc5245e06c5d
lilianaromerop <lilianaromerop11@gmail.com>
2022-05-13 22:08:18 -0500

    cuadrando footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,23 +11,23 @@ export default function Home() {
       <div>
           <Box sx={{backgroundColor:"#F9F7F6"}}id="about">
             <Container maxWidth="xl">
-                <div className="mt-20 flex justify-center space-x-32">
+                <div className="flex mt-20 justify-center space-x-32">
                   <div>
-                  <span className='text-gray-400 font-semibold text-xl'>Sobre Nosotros</span>
-                      <Typography fontFamily={'Quicksand'} sx={{color:'#4A4C70'}} className='text-5xl font-extrabold'>Soy Tú Salud</Typography>
-                    <div>
-                      <p className="max-w-2xl mt-8 mb-4">Conoces a algún familiar, amigo, persona o tú que necesite algún servicio médico y se encuentre en situación de vulnerabilidad? En www.soytusalud.org informa tu historia o su historia, aplica y Listo!!.<p> <br/>En la Fundación <a href="">www.soytusalud.org</a> una vez has demostrado que estás en una situación de vulnerabilidad, podrás acceder :</p></p>
-                        <ul className="list-disc list-inside">
-                          <li>Consulta Médica General por Telemedicina.</li>
-                          <li>Consulta Médica Especializada por Telemedicina.</li>
-                          <li>Medicamentos para enfermedades no crónicas.</li>
-                          <li>Exámenes de Laboratorio Domiciliarios.</li>
-                          <li>Otros servicios para su salud, que estén disponibles.</li>
-                        </ul>
-                        <button className="mt-10 border-2 border-amber-300 hover:bg-amber-300 hover:drop-shadow-md rounded-full p-4 cursor-pointer px-12 uppercase text-xs font-bold" href="#">Más sobre nosotros</button>
-                    </div>
+                    <span className='text-gray-400 font-semibold text-xl'>Sobre Nosotros</span>
+                        <Typography fontFamily={'Quicksand'} sx={{color:'#4A4C70'}} className='text-5xl font-extrabold'>Soy Tú Salud</Typography>
+                      <div>
+                        <p className="max-w-2xl mt-8 mb-4">Conoces a algún familiar, amigo, persona o tú que necesite algún servicio médico y se encuentre en situación de vulnerabilidad? En www.soytusalud.org informa tu historia o su historia, aplica y Listo!!.<p> <br/>En la Fundación <a href="">www.soytusalud.org</a> una vez has demostrado que estás en una situación de vulnerabilidad, podrás acceder :</p></p>
+                          <ul className="list-disc list-inside">
+                            <li>Consulta Médica General por Telemedicina.</li>
+                            <li>Consulta Médica Especializada por Telemedicina.</li>
+                            <li>Medicamentos para enfermedades no crónicas.</li>
+                            <li>Exámenes de Laboratorio Domiciliarios.</li>
+                            <li>Otros servicios para su salud, que estén disponibles.</li>
+                          </ul>
+                          <button className="mt-10 border-2 border-amber-300 hover:bg-amber-300 hover:drop-shadow-md rounded-full p-4 cursor-pointer px-12 uppercase text-xs font-bold" href="#">Más sobre nosotros</button>
+                      </div>
                   </div>
-                  <div className="flex-col">
+                  <div className="">
                     <div className="absolute">
                       <Image src="/about_layout.png" alt="img" width={650} height={650}/>
                     </div>
