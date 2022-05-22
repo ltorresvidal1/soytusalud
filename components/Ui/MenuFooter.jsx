8f3112a1a2c8bbbc8125b1468d744e400b134235
lilianaromerop <lilianaromerop11@gmail.com>
2022-05-21 22:06:27 -0500

    form trabaja con nosotros

---
+++ components/Ui/MenuFooter.jsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import LogoInferior from '../../assets/images/Logo_P4.png';
+
+export const MenuFooter = () => {
+  return (
+
+    <footer className="footer">
+    <div className="container">
+        <div className="row">
+            <div className="col-sm-6 col-lg-2">
+                 <div className='text-center'>
+                     <div className="footer-logo"><a className="footer-logo__link"><Image className="" src={LogoInferior}  width="150" height="150" alt="logo"/></a></div>
+                      </div>
+            
+            
+                
+                <ul className="footer-socials">
+                    <li className="footer-socials__item"><a className="footer-socials__link" href="https://www.facebook.com/Fundación-Soy-Tú-Salud-107327561900263" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li className="footer-socials__item"><a className="footer-socials__link" href="https://www.linkedin.com/in/fundación-soy-tú-salud-845592233/" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li className="footer-socials__item"><a className="footer-socials__link" href="https://twitter.com/FundacinSoyTSa1" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li className="footer-socials__item"><a className="footer-socials__link" href="https://www.instagram.com/fundacionsoytusalud/" target="_blank"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+                </ul>
+                
+                
+            </div>
+            <div className="col-sm-6 col-lg-3">
+                <h4 className="footer__title">Contactos</h4>
+                <div className="footer-contacts">
+                    <p className="footer-contacts__address">Medellin</p>
+                    <p className="footer-contacts__phone">Telefono: <a href="tel:+3207148401">+57 320 702 3823</a></p>
+                    <p className="footer-contacts__mail">Email: <a href="mailto:soutu@soutu.org">info@soytusalud.org</a></p>
+                </div>
+            </div>
+            <div className="col-sm-6 col-lg-3">
+                
+                
+                
+                
+            </div>
+            <div className="col-sm-6 col-lg-3">
+                <h4 className="footer__title">Te Acompaño</h4>
+                <p>Ayúdanos a cambiar la vida de los menos favorecidos en Colombia</p><a className="button footer__button button--filled" href="#">TE ACOMPAÑO</a>
+            </div>
+        </div>
+        <div className="row align-items-baseline">
+            <div className="col-md-6">
+                <p className="footer-copyright">Copyrights Soy Tú © 2022. Todos los derechos reservado.</p>
+            </div>
+            
+        </div>
+    </div>
+</footer>
+  )
+}
---
+++ components/Ui/MenuHead.jsx
@@ -0,0 +1,151 @@
+import Image from 'next/image';
+import {useState} from 'react'
+import LogoDark from '../../assets/images/logo_dark.png';
+import LogoWhite from '../../assets/images/logo_white.png';
+import Link from 'next/link'
+
+
+export const MenuHead = () => {
+    const [open, setOpen] = useState(false)
+  return (
+   
+		<>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@319;400;600;700&display=swap" rel="stylesheet"></link>
+
+        {/* barra oculta */}
+        {/* {open?(
+            <div>
+                    <h1>oe</h1>
+                    <h1>oe</h1>
+            </div>
+        ):null} */}
+            <header className="header header--front">
+            <div className="container-fluid">
+                <div className="row no-gutters justify-content-between">
+                    <div className="col-auto d-flex align-items-center" >
+                        <div className="dropdown-trigger d-none d-sm-block" >
+                            <div className=""></div>
+                        </div>
+                        <div className="header-logo">
+                            <a className="header-logo__link" href="index.php"></a>
+                            <Image className="header-logo__img logo--light" src={LogoWhite} alt="logo"/>
+                        </div>
+                    </div>
+                    <div className="col-auto">
+                        <nav>
+                            <ul className="main-menu">
+                                <li className="main-menu__item main-menu__item--active">
+                                    <Link href="/tuhistoria" > 
+                                    <a className="main-menu__link">Inicio</a>
+                                    </Link>
+                                </li>
+                               
+                
+                                    <li className="main-menu__item main-menu__item--has-child">
+                                            <Link href="/" > 
+                                                <a className="main-menu__link">Pacientes</a>
+                                            </Link> 
+                                            <ul className="main-menu__sub-list">
+                                        
+                                            <li>    
+                                                <Link href="/" > 
+                                                    <a>Inicio de sessión</a>
+                                                </Link>
+                                               
+                                            </li>
+                                            <li>    
+                                                <Link href="/" > 
+                                                    <a>Registrarme</a>
+                                                </Link>
+                                               
+                                            </li>
+                                            <li>    
+                                                <Link href="/" > 
+                                                    <a>Tu historia</a>
+                                                </Link>
+                                               
+                                            </li>
+                                            <li>    
+                                                <Link href="/" > 
+                                                    <a>Cerrar sesisión</a>
+                                                </Link>
+                                               
+                                            </li>                                       
+                                            
+                                          
+                                            </ul>
+                                    </li>	
+                                            
+                                            <li className="main-menu__item main-menu__item--has-child">
+                                            <Link href="/" > 
+                                                            <a className="main-menu__link">Comunidades E Instituciones</a>
+                                            </Link> 
+                                               
+                                    
+                                            <ul className="main-menu__sub-list">
+                                                    <li>    
+                                                        <Link href="/" > 
+                                                            <a>Registro</a>
+                                                        </Link>   
+                                                    </li> 
+                                                    <li>    
+                                                        <Link href="/" > 
+                                                            <a>Inscribir Comunidad</a>
+                                                        </Link>   
+                                                    </li> 
+                                                    <li>    
+                                                        <Link href="/" > 
+                                                            <a>Inscribir Institución</a>
+                                                        </Link>   
+                                                    </li> 
+                                                    <li>    
+                                                        <Link href="/" > 
+                                                            <a>Preleccionar Beneficiarios</a>
+                                                        </Link>   
+                                                    </li> 
+                                                    <li>    
+                                                        <Link href="/" > 
+                                                            <a>Seguimiento de Servicios</a>
+                                                        </Link>   
+                                                    </li> 
+                                                                                    
+                                                
+                                            </ul>
+                                  
+                                </li>
+                                
+                                
+                                <li className="main-menu__item">
+                                           <Link href="/" > 
+                                                <a className="main-menu__link">Filantropos</a>
+                                            </Link>                                                           
+                                </li>
+                                
+                            </ul>
+                        </nav>
+                       
+                    </div>
+                    <div className="col-auto d-flex align-items-center">
+                        <ul className="lang-select lang-select--inner">
+                            <li className="lang-select__item lang-select__item--active"><span data-lang="idioma">Es</span>
+                                <ul className="lang-select__sub-list">
+                                    
+                                        <li><a href="#" id="translate" data-text="English,Espa&ntilde;ol" data-file="es,en" data-index="1">English</a></li>
+                                    
+                                </ul>
+                            </li>
+                        </ul>
+                       
+                        {/* <div className="dropdown-trigger d-block d-sm-none" onClick={()=>setOpen(!open)}>
+                            <div className="dropdown-trigger__item"></div>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        </header>
+        </>
+  )
+}
+
